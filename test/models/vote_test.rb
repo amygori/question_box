16b56c22ab2d1c6d1d102221c6bb3bcf19299b78
amygori <amydgori@gmail.com>
2014-10-16 10:03:48 -0400

    added test and logic for 10 points awarded to user for answer votes

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -20,11 +20,21 @@ class VoteTest < ActiveSupport::TestCase
     assert_not_empty @vote.errors[:value]
   end
 
-  test "should add 10 points to question or answer user" do
+  test "should add 10 points to question user" do
     question = questions(:one)
     previous_score = question.user.score
     question.votes.create(value:1)
+    question.user.reload
     puts "question score #{question.user.score}"
     assert_equal 10, question.user.score - previous_score
+  end
+
+  test "should add 10 points to answer user" do
+    answer = answer(:one)
+    previous_score = answer.user.score
+    answer.votes.create(value:1)
+    answer.user.reload
+    puts "answer score #{answer.user.score}"
+    assert_equal 10, answer.user.score - previous_score
   end
 end
